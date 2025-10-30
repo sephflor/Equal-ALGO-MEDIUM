99cf78146d4fb05cafc6e3e3306817915b8ee533
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-30 19:15:08 +0800

    Implement equal function to minimize operations

---
+++ Equal.cs
@@ -0,0 +1,74 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'equal' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equal(List<int> arr)
+    {
+         int min = arr.Min();
+        int result = int.MaxValue;
+
+        
+        for (int baseValue = min; baseValue >= min - 5; baseValue--)
+        {
+            int operations = 0;
+            foreach (int a in arr)
+            {
+                int delta = a - baseValue;
+                operations += delta / 5;
+                delta %= 5;
+                operations += delta / 2;
+                delta %= 2;
+                operations += delta;
+            }
+            result = Math.Min(result, operations);
+        }
+
+        return result;
+    }
+
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = Result.equal(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
